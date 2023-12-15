b5c8d5917ad0e6f0dcb40a53b7eedb324e7aaa71
Yusef-H <yusefhalabi456@gmail.com>
2023-12-15 20:44:42 +0200

    add mongodb connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 
 const PORT = process.env.PORT || 3333;
@@ -9,6 +11,12 @@ app.use(cors());
 
 app.listen(PORT, (req, res) => {
     console.log(`Server Running on Port: ${PORT}`);
+})
+
+mongoose.connect(process.env.CONNECT_STR).then(() => {
+    console.log("connected to mongodb");
+}).catch((err) => {
+    console.log("connection failed " + err.message)
 })
 
 module.exports = app;
