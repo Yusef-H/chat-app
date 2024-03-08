3c207a46c788674931b8c7a8624f9ff098988f1c
Yusef-H <yusefhalabi456@gmail.com>
2024-03-08 21:15:03 +0200

    click on main title removes nav expand

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import '../styles/navbar.css'
 import menuIcon from '../utils/menuIcon.svg';
-import {useState} from 'react';
+import { useState } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 function Navbar() {
     const [isNavExpanded, setIsNavExpanded] = useState(false);
@@ -8,20 +8,20 @@ function Navbar() {
     return <>
         <nav className='navbar'>
             <div className="main">
-                <Link to="/">Chat</Link>
+                <Link to="/" onClick={() => setIsNavExpanded(false)}>Chat</Link>
             </div>
 
             <button className="menu"
-                onClick={() => {setIsNavExpanded(!isNavExpanded)}} >
+                onClick={() => { setIsNavExpanded(!isNavExpanded) }} >
                 <img src={menuIcon} alt="Menu" />
             </button>
 
             <span className={isNavExpanded ? 'name expanded' : 'name'}>Name : Yusef</span>
             <div className={isNavExpanded ? 'links expanded' : 'links'}>
-                <Link to="/login" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Login</Link>
-                <Link to="/register" onClick={() => {setIsNavExpanded(!isNavExpanded)}}>Register</Link>
+                <Link to="/login" onClick={() => { setIsNavExpanded(!isNavExpanded) }}>Login</Link>
+                <Link to="/register" onClick={() => { setIsNavExpanded(!isNavExpanded) }}>Register</Link>
             </div>
-        </nav>
+        </nav >
         <Outlet />
     </>
 }
